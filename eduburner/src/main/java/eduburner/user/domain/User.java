ad83f8741254e843f95d43a94d96fa6edbae41e8
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-08-28 15:48:20 +0000

    add new user and roles

---
+++ eduburner/src/main/java/eduburner/user/domain/User.java
@@ -0,0 +1,52 @@
+package eduburner.user.domain;
+
+import org.springframework.security.GrantedAuthority;
+import org.springframework.security.userdetails.UserDetails;
+
+public class User implements UserDetails {
+
+	private static final long serialVersionUID = 3619044126921173168L;
+
+	@Override
+	public GrantedAuthority[] getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
