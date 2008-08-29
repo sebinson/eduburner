31d1f7968293c35ec3ad9215184b403e7ecf518e
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-08-29 14:15:59 +0000

    update user and role

--- eduburner/src/main/java/eduburner/user/domain/User.java
+++ eduburner/src/main/java/eduburner/user/domain/User.java
@@ -1,61 +1,185 @@
 package eduburner.user.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+import javax.persistence.UniqueConstraint;
+import javax.persistence.Version;
+
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.hibernate.validator.Length;
+import org.hibernate.validator.NotNull;
 import org.springframework.security.GrantedAuthority;
 import org.springframework.security.userdetails.UserDetails;
 
 import eduburner.core.EntityObject;
 
+@Entity
+@Table(name = "user", uniqueConstraints = { @UniqueConstraint(columnNames = {
+		"username", "email" }) })
 public class User extends EntityObject implements UserDetails {
 
 	private static final long serialVersionUID = 3619044126921173168L;
-	
+
 	private String username;
 
-	@Override
-	public GrantedAuthority[] getAuthorities() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private String password;
 
-	@Override
-	public String getPassword() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	private String confirmPassword;
 
-	@Override
+	private String fullname;
+
+	private String email;
+
+	private Integer version;
+
+	private Set<Role> roles = new HashSet<Role>(0);
+
+	private boolean enabled = true;
+
+	private boolean accountExpired = false;
+
+	private boolean accountLocked = false;
+
+	private boolean credentialsExpired = false;
+
+	@NotNull(message = "用户名不能为空！")
+	@Length(min = 3, max = 16, message = "用户名最少 {min}, 最大 {max} 个字符")
+	@Column(name = "username", length = 50, unique = true, nullable = false)
 	public String getUsername() {
-		// TODO Auto-generated method stub
-		return null;
+		return username;
 	}
 
-	@Override
-	public boolean isAccountNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+	@Column(name = "password", length = 50, nullable = false)
+	public String getPassword() {
+		return password;
 	}
 
-	@Override
-	public boolean isAccountNonLocked() {
-		// TODO Auto-generated method stub
-		return false;
+	@Version
+	public Integer getVersion() {
+		return version;
 	}
 
-	@Override
-	public boolean isCredentialsNonExpired() {
-		// TODO Auto-generated method stub
-		return false;
+	@Column(name = "fullname")
+	public String getFullname() {
+		return fullname;
 	}
 
-	@Override
+	@Column(name = "email")
+	public String getEmail() {
+		return email;
+	}
+
+	@Column(name = "enabled", nullable = false)
 	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return false;
+		return enabled;
 	}
 
-	@Override
+	@Column(name = "account_expired")
+	public boolean isAccountExpired() {
+		return accountExpired;
+	}
+
+	@Transient
+	public boolean isAccountNonExpired() {
+		return !isAccountExpired();
+	}
+
+	@Column(name = "account_locked")
+	public boolean isAccountLocked() {
+		return accountLocked;
+	}
+
+	@Transient
+	public boolean isAccountNonLocked() {
+		return !isAccountLocked();
+	}
+
+	@Column(name = "credentials_expired")
+	public boolean isCredentialsExpired() {
+		return credentialsExpired;
+	}
+
+	@Transient
+	public boolean isCredentialsNonExpired() {
+		return !isCredentialsExpired();
+	}
+
+	@Transient
+	public GrantedAuthority[] getAuthorities() {
+		return roles.toArray(new GrantedAuthority[0]);
+	}
+
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER)
+	@JoinTable(name = "user_role", joinColumns = { @JoinColumn(name = "user_id") }, inverseJoinColumns = { @JoinColumn(name = "role_id") })
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setAccountExpired(boolean accountExpired) {
+		this.accountExpired = accountExpired;
+	}
+
+	public void setAccountLocked(boolean accountLocked) {
+		this.accountLocked = accountLocked;
+	}
+
+	public void setCredentialsExpired(boolean credentialsExpired) {
+		this.credentialsExpired = credentialsExpired;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	@Transient
+	public String getConfirmPassword() {
+		return confirmPassword;
+	}
+
+	public void setConfirmPassword(String confirmPassword) {
+		this.confirmPassword = confirmPassword;
+	}
+
+	public void addRole(Role role) {
+		this.roles.add(role);
+	}
+
 	public String toString() {
-		return this.username;
+		return new ToStringBuilder(this).append("username", username).append(
+				"password", password).toString();
 	}
-
 }
