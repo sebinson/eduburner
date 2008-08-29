b8f90ecf0e63c7ee863d1c001a59ecbe09872eaf
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-08-29 14:15:59 +0000

    update user and role

--- trunk/eduburner/src/main/java/eduburner/user/domain/Role.java
+++ trunk/eduburner/src/main/java/eduburner/user/domain/Role.java
@@ -1,28 +1,73 @@
 package eduburner.user.domain;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
+
 import org.springframework.security.GrantedAuthority;
 
 import eduburner.core.EntityObject;
 
+@Entity
 public class Role extends EntityObject implements GrantedAuthority {
-
 	private static final long serialVersionUID = -908605749081541265L;
-	
+
 	private String name;
+	
+	private String description;
+
+	protected Set<User> users = new HashSet<User>();
+
+	@Column(name = "name", nullable = false, unique = true)
+	public String getName() {
+		return name;
+	}
+	
+	public String getDescription() {
+		return description;
+	}
+
+	@ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE }, fetch = FetchType.EAGER, mappedBy = "roles")
+	public Set<User> getUsers() {
+		return users;
+	}
 
 	@Override
 	public String getAuthority() {
 		return this.name;
 	}
 
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setUsers(Set<User> users) {
+		this.users = users;
+	}
+	
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	@Override
 	public int compareTo(Object o) {
-		return 0;
+		if(!(o instanceof Role)){
+			throw new IllegalArgumentException();
+		}
+		Role role = (Role)o;
+		if(role.getName().equals(this)){
+			return 0;
+		}
+		return 1;
 	}
 
 	@Override
 	public String toString() {
 		return this.name;
 	}
-
 }
