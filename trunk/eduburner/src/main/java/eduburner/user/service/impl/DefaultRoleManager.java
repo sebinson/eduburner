4fe7ff1f139dfc8c22fb280c73855887f31139a2
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-09-04 00:26:28 +0000

    update dao

--- trunk/eduburner/src/main/java/eduburner/user/service/impl/DefaultRoleManager.java
+++ trunk/eduburner/src/main/java/eduburner/user/service/impl/DefaultRoleManager.java
@@ -1,18 +1,14 @@
 package eduburner.user.service.impl;
 
-import java.util.List;
-
 import javax.persistence.EntityExistsException;
 
 import org.hibernate.criterion.DetachedCriteria;
 import org.hibernate.criterion.Restrictions;
-import org.springframework.security.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import eduburner.core.service.BaseManager;
 import eduburner.user.domain.Role;
 import eduburner.user.service.IRoleManager;
-
 
 @Service("roleManager")
 public class DefaultRoleManager extends BaseManager implements IRoleManager {
@@ -21,23 +17,14 @@ public class DefaultRoleManager extends BaseManager implements IRoleManager {
 		return dao.getInstanceById(Role.class, id);
 	}
 
-	@SuppressWarnings("unchecked")
 	public Role getRoleByName(String rolename) {
 		DetachedCriteria criteria = DetachedCriteria.forClass(Role.class);
 		criteria.add(Restrictions.eq("name", rolename));
-
-		List roles = dao.getInstancesByDetachedCriteria(criteria);
-
-		if (roles.size() == 0) {
-			throw new UsernameNotFoundException("Role not found");
-		}
-
-		return (Role) roles.get(0);
+		return (Role) dao.getUniqueInstanceByDetachedCriteria(criteria);
 	}
 
 	public void removeRole(String rolename) {
 		dao.remove(getRoleByName(rolename));
-
 	}
 
 	public void saveRole(Role role) {
