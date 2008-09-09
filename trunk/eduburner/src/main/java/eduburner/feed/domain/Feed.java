1bf1fc60833cb7a8eb807fe97a4fa3963ed3486c
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-09-09 14:51:14 +0000

    add crawler and feed

---
+++ trunk/eduburner/src/main/java/eduburner/feed/domain/Feed.java
@@ -0,0 +1,13 @@
+package eduburner.feed.domain;
+
+import eduburner.core.EntityObject;
+
+public class Feed extends EntityObject {
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
