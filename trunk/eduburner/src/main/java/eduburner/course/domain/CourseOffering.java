7f2a5a9aecb8b522d6eac870d755feb9e28f57df
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-09-11 16:26:53 +0000

    add comment

--- trunk/eduburner/src/main/java/eduburner/course/domain/CourseOffering.java
+++ trunk/eduburner/src/main/java/eduburner/course/domain/CourseOffering.java
@@ -1,29 +1,47 @@
 package eduburner.course.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.commons.lang.builder.ToStringBuilder;
 
 import eduburner.core.EntityObject;
+import eduburner.user.domain.UserData;
 
 /**
  * 课程类，一门课程有多个学生，有讨论区和共享资源
+ * 
+ * 能mashup的就mashup
+ * 
+ * 日历借助google calendar
+ * 每个人的学习笔记借助google notebook
+ * 文章分享借助rss
+ * 
+ * 课程需要一个论坛
+ * 
+ * 每位同学是否需要有博客？
+ * 是否需要建立好友关系？
+ * 
  * @author zhangyf@gmail.com
  */
 public class CourseOffering extends EntityObject {
 
 	private static final long serialVersionUID = -1025308879798159789L;
-	
+
 	private String title;
-	
+
 	private String description;
-	
-	//This might be open, closed, planned, or discontinued, for example
+
+	// This might be open, closed, planned, or discontinued, for example
 	private String status;
-	
+
 	private Date startDate;
-	
+
 	private Date endDate;
+
+	//成员
+	private List<UserData> members = new ArrayList<UserData>();
 
 	@Override
 	public String toString() {
