f015261b599e0c8cb192c6c15f5cb4bfefce0932
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-09-12 00:42:07 +0000

    add comment

--- eduburner/src/main/java/eduburner/section/domain/Post.java
+++ eduburner/src/main/java/eduburner/section/domain/Post.java
@@ -4,6 +4,8 @@ import eduburner.core.EntityObject;
 
 public class Post extends EntityObject {
 
+	private static final long serialVersionUID = 697717739347893427L;
+
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
