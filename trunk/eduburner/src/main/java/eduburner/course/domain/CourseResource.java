24467c74c75cfc95aa70a8c97cdedb3e56412b60
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-11 14:50:07 +0000

    update course

--- trunk/eduburner/src/main/java/eduburner/course/domain/CourseResource.java
+++ trunk/eduburner/src/main/java/eduburner/course/domain/CourseResource.java
@@ -1,21 +1,44 @@
 package eduburner.course.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 import eduburner.core.EntityObject;
 
 /**
  * 课程资源，上传的文档之类
  * @author zhangyf@gmail.com
  */
+
+@Entity
+@Table(name="course_resource")
 public class CourseResource extends EntityObject {
 
 	private static final long serialVersionUID = -7991836659179446511L;
 	
-	
+	private String name;
 
+	private CourseOffering course;
+
+	public CourseOffering getCourse() {
+		return course;
+	}
+
+	public void setCourse(CourseOffering course) {
+		this.course = course;
+	}
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
 	@Override
 	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+		return name;
 	}
 
 }
