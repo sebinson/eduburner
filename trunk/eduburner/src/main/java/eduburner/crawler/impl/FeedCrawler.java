d647ea4673ef44d97713dd7ef23c128ccfe420cc
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-13 00:43:47 +0000

    update crawler

---
+++ trunk/eduburner/src/main/java/eduburner/crawler/impl/FeedCrawler.java
@@ -0,0 +1,7 @@
+package eduburner.crawler.impl;
+
+import eduburner.crawler.IFeedCrawler;
+
+public class FeedCrawler implements IFeedCrawler {
+
+}
---
+++ trunk/eduburner/src/main/java/eduburner/user/domain/UserSetting.java
@@ -0,0 +1,13 @@
+package eduburner.user.domain;
+
+import eduburner.core.EntityObject;
+
+public class UserSetting extends EntityObject{
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
