5bd2e7057b133321ebf898fd9bb43f279fe9660c
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-15 00:53:36 +0000

    update spring configuration

--- trunk/eduburner/src/main/java/eduburner/course/service/impl/DefaultCourseOfferingManager.java
+++ trunk/eduburner/src/main/java/eduburner/course/service/impl/DefaultCourseOfferingManager.java
@@ -1,8 +1,17 @@
 package eduburner.course.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import eduburner.core.service.BaseManager;
+import eduburner.course.domain.CourseOffering;
 import eduburner.course.service.ICourseOfferingManager;
 
+@Service("courseManager")
 public class DefaultCourseOfferingManager extends BaseManager implements ICourseOfferingManager {
+
+	@Override
+	public void addCourse(CourseOffering course) {
+		
+	}
 
 }
