cbcc0af45d1b86b5e0381680267d3026cd12d6ec
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-16 14:56:49 +0000

    update feed crawler

--- trunk/eduburner/src/main/java/eduburner/feed/fetcher/IFeedCrawler.java
+++ trunk/eduburner/src/main/java/eduburner/feed/fetcher/IFeedCrawler.java
@@ -3,5 +3,10 @@ package eduburner.feed.fetcher;
 import eduburner.feed.domain.Feed;
 
 public interface IFeedCrawler {
+	
+	public boolean start();
+	
+	public boolean stop();
+	
 	public void crawle(Feed feed);
 }
