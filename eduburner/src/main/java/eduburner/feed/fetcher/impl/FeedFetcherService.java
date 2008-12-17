f367a6fd09dbb19f1c1b89391c119d3f859c2c92
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-17 14:29:41 +0000

    update fetcher code

---
+++ eduburner/src/main/java/eduburner/feed/fetcher/impl/FeedFetcherService.java
@@ -0,0 +1,7 @@
+package eduburner.feed.fetcher.impl;
+
+import eduburner.feed.fetcher.IFeedFetcherService;
+
+public class FeedFetcherService implements IFeedFetcherService{
+	
+}
---
+++ eduburner/src/main/java/eduburner/feed/service/impl/FeedManager.java
@@ -0,0 +1,10 @@
+package eduburner.feed.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import eduburner.feed.service.IFeedManager;
+
+@Service("feedManager")
+public class FeedManager implements IFeedManager{
+	
+}
