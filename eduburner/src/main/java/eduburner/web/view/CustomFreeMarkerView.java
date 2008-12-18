93d8fa4f830c563ee4c9dea5c36e0d96a395eb37
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-18 13:58:08 +0000

    update config

---
+++ eduburner/src/main/java/eduburner/web/view/CustomFreeMarkerView.java
@@ -0,0 +1,34 @@
+package eduburner.web.view;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerView;
+
+import eduburner.util.RequestUtils;
+import eduburner.util.SecurityHelper;
+
+/**
+ * add base and securityHelper to model, so it can be accessed. e.g.
+ * ${securityHelper.principal?default("")}
+ * 
+ * @author rockmaple
+ * 
+ */
+public class CustomFreeMarkerView extends FreeMarkerView {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(CustomFreeMarkerView.class);
+
+	@SuppressWarnings("unchecked")
+	@Override
+	protected void exposeHelpers(Map model, HttpServletRequest request)
+			throws Exception {
+		model.put("base", RequestUtils.getResourceBase(request));
+		model.put("securityHelper", new SecurityHelper());
+		model.put("req", request);
+	}
+}
