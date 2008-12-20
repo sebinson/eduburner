d2eeceff4d3d858ea65bdc6bc795bc992888b851
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-20 10:13:12 +0000

    code refactor

---
+++ trunk/eduburner/src/main/java/eduburner/entry/conrollers/EntryController.java
@@ -0,0 +1,14 @@
+package eduburner.entry.conrollers;
+
+import eduburner.web.controllers.BaseController;
+
+public class EntryController extends BaseController{
+
+	public String index(){
+		return null;
+	}
+	
+	public String editNew(){
+		return null;
+	}
+}
