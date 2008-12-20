dc7c6cafe5104ae7ccffab24ddd1469935e87715
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2008-12-20 03:35:25 +0000

    update feed entities

--- eduburner/src/main/java/eduburner/feed/domain/Comment.java
+++ eduburner/src/main/java/eduburner/feed/domain/Comment.java
@@ -6,23 +6,29 @@ import eduburner.core.EntityObject;
 import eduburner.user.domain.UserData;
 
 public class Comment extends EntityObject {
-	private static final long serialVersionUID = -8070149621622984770L;
+	private static final long serialVersionUID = 6416275923661542381L;
 	private Date date;
+	private String commentId;
 	private UserData user;
 	private String body;
 
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return null;
+	public Comment() {
 	}
 
 	public Date getDate() {
 		return date;
 	}
 
+	public String getBody() {
+		return body;
+	}
+
 	public void setDate(Date date) {
 		this.date = date;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
 	}
 
 	public UserData getUser() {
@@ -33,12 +39,17 @@ public class Comment extends EntityObject {
 		this.user = user;
 	}
 
-	public String getBody() {
-		return body;
+	public String toString() {
+		return "Comment{" + "date=" + date + ", id='" + id + '\'' + ", user="
+				+ user + ", body='" + body + '\'' + '}';
 	}
 
-	public void setBody(String body) {
-		this.body = body;
+	public String getCommentId() {
+		return commentId;
+	}
+
+	public void setCommentId(String commentId) {
+		this.commentId = commentId;
 	}
 
 }
---
+++ eduburner/src/main/java/eduburner/feed/domain/Via.java
@@ -0,0 +1,25 @@
+package eduburner.feed.domain;
+
+import eduburner.core.EntityObject;
+
+public class Via extends EntityObject{
+    private static final long serialVersionUID = 6794354127987399201L;
+	private String name;
+    private String url;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public String toString() {
+        return "Via{" +
+                "name='" + name + '\'' +
+                ", url='" + url + '\'' +
+                '}';
+    }
+}
