3e6a2823398c2608509dd61ecc76298c05a3084d
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-02-18 14:25:13 +0000

    update scripts

--- eduburner/src/main/webapp/scripts/main.js
+++ eduburner/src/main/webapp/scripts/main.js
@@ -1,12 +1,35 @@
 (function($){
 	
-	var PageManager = new function(){
-		
-		this.onDomReady = function(){
-			
-		};
+	var Env = {
+		_prefix : 'id',
+		_uidx : 0
 	};
 	
-	window.PageMgr = PageManager;
-	
+	var Utility = new function(){
+		
+		this.guid = function(prefix){
+			var p = prefix || Env._prefix;
+			return p + '-' + Env._uidx++;	
+		};
+		
+	};
+
+    var PageMgr = new function(){
+		
+		var setBodyClass = function(){
+			var $bd = $('body');
+			var cls = $.browser.msie ? "ie " + ($.browser.version == '6.0' ? 'ie6' : 'ie7')
+			            : $.browser.mozilla ? "mozilla"
+			            : $.browser.opera ? "opera"
+			            : $.browser.safari ? "safari" : "";            
+            $bd.addClass(cls);
+		};
+    
+        this.onDomReady = function(){
+            setBodyClass();
+        };
+    };
+    
+    window.PageMgr = PageMgr;
+	window.Utility = Utility;
 })(jQuery);
