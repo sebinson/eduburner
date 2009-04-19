e17ced43cc5306408b0c8394385bc64fc96c01a6
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-04-19 13:01:45 +0000

    update crawler

--- eduburner-modular/EduBurner-Modular/EduBurner/crawler/src/main/java/eduburner/crawler/CrawlController.java
+++ eduburner-modular/EduBurner-Modular/EduBurner/crawler/src/main/java/eduburner/crawler/CrawlController.java
@@ -15,7 +15,7 @@ import eduburner.crawler.processor.IProcessor;
 /**
  * crawler module for eduburner. 
  * 
- * 实现原理和命名方式参考 heritrix <a href="http://crawler.archive.org/">http://crawler.archive.org/</a>
+ * 包eduburner.crawler下的代码，实现原理和命名方式参考 heritrix <a href="http://crawler.archive.org/">http://crawler.archive.org/</a>
  * 
  * 部分代码也来自于 heritrix
  * 
