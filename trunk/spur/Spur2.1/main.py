cf25f66ba3aecce7641b3a0e1509cbd5cb3caaf2
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-07-21 15:36:25 +0000

    add spur codes

---
+++ trunk/spur/Spur2.1/main.py
@@ -0,0 +1,38 @@
+#coding=utf-8
+
+import logging
+import wsgiref.handlers
+
+from google.appengine.ext import webapp
+
+from app.framework import config
+from app.framework import dispatcher
+from app.framework import mapper
+
+from app.controllers import *
+
+
+def initRoutes():
+    m = mapper.Mapper()
+    m.connect('/entries/',                      controller = EntryController)
+    m.connect('/tags/{tag_key_name}/entries',    controller = EntryController)
+    m.connect('/resources/',                    controller = ResourceController)
+    m.connect('/tags/{tag_key_name}/resources',  controller = ResourceController)
+    m.connect('/entrytags/',                    controller = EntryTagController)
+    m.connect('/resourcetags/',                 controller = ResTagController)
+    m.connect('/projects/',                     controller = ProjectController)
+    m.connect('/admin/',                        controller = AdminController)
+        
+def main():
+    logging.getLogger().setLevel(settings.LOGGING_LEVEL)
+    
+    cfg = config.Config()
+    cfg.template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+
+    initMapper()
+    
+    application = webapp.WSGIApplication([(r'.*', dispatcher.RestHandler)], debug=config.DEBUG)
+    wsgiref.handlers.CGIHandler().run(application)
+
+if __name__ == '__main__':
+    main()
