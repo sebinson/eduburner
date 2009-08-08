7eb95179c3f1f05a8d4c31ba7b1e692494e954ef
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-08-08 09:35:58 +0000

    code refactor

--- eduburner-modular/EduBurner-Modular/EduBurner/crawler/src/main/java/eduburner/crawler/frontier/AbstractFrontier.java
+++ eduburner-modular/EduBurner-Modular/EduBurner/crawler/src/main/java/eduburner/crawler/frontier/AbstractFrontier.java
@@ -1,73 +1,11 @@
 package eduburner.crawler.frontier;
 
-import eduburner.crawler.model.CrawlURI;
+import java.io.Serializable;
 
-public class AbstractFrontier implements IFrontier {
+public abstract class AbstractFrontier implements IFrontier, Serializable {
 
-	@Override
-	public long failedFetchCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+	private static final long serialVersionUID = -8091508743170194678L;
 
-	@Override
-	public void finished(CrawlURI uri) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void initTasks() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean isEmpty() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public CrawlURI next() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void pause() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public long queuedUriCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void resume() {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void schedule(CrawlURI uri) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public long succeededFetchCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public void terminate() {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
--- eduburner-modular/EduBurner-Modular/EduBurner/crawler/src/main/java/eduburner/crawler/frontier/WorkQueue.java
+++ eduburner-modular/EduBurner-Modular/EduBurner/crawler/src/main/java/eduburner/crawler/frontier/WorkQueue.java
@@ -1,8 +1,9 @@
 package eduburner.crawler.frontier;
 
+import java.io.IOException;
 import java.io.Serializable;
-import java.util.Queue;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.Delayed;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,45 +15,161 @@ import eduburner.crawler.model.CrawlURI;
  * "hostname:port" or similar)
  * 
  */
-public class WorkQueue implements Serializable {
+public abstract class WorkQueue implements Serializable, Comparable<Delayed>,
+		Delayed {
 
 	private static final long serialVersionUID = 2941833658867158238L;
 
 	private static final Logger logger = LoggerFactory
 			.getLogger(WorkQueue.class);
 
-	private String classKey;
-	private long wakeTime;
+	protected String classKey;
+	protected long wakeTime;
 
-	private Queue<CrawlURI> uriQueue = new LinkedBlockingQueue<CrawlURI>();
+	/** whether queue is active (ready/in-process/snoozed) or on a waiting queue */
+	private boolean active = true;
+	/** Total number of stored items */
+	private long count = 0;
+	/** Total number of items ever enqueued */
+    private long enqueueCount = 0;
+	/** The next item to be returned */
+	protected CrawlURI peekItem = null;
+	/** Last URI enqueued */
+	private String lastQueued;
+	/** Last URI peeked */
+	private String lastPeeked;
+	/** time of last dequeue (disposition of some URI) **/
+	private long lastDequeueTime;
+	/** count of errors encountered */
+	private long errorCount = 0;
 
-	public WorkQueue(String classKey) {
+	public WorkQueue(final String classKey) {
 		this.classKey = classKey;
-		this.wakeTime = System.currentTimeMillis();
 	}
 
-	public void addUri(CrawlURI uri) {
-		if (this.classKey.equals(uri.getClassKey())) {
-			logger.debug("add uri to work queue");
-			uriQueue.add(uri);
-		} else {
-			logger.debug("uri do not belong to this queue.");
+    /**
+     * Add the given CrawlURI, noting its addition in running count. (It
+     * should not already be present.)
+     * 
+     * @param frontier Work queues manager.
+     * @param curi CrawlURI to insert.
+     */
+    protected void enqueue(final WorkQueueFrontier frontier,
+        CrawlURI curi) {
+        try {
+            insert(frontier, curi, false);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+        count++;
+        enqueueCount++;
+    }
+    
+	public CrawlURI peek(final WorkQueueFrontier frontier) {
+		if (peekItem == null && count > 0) {
+			try {
+				peekItem = peekItem(frontier);
+			} catch (IOException e) {
+				logger.warn("peek failure", e);
+				e.printStackTrace();
+			}
+			if (peekItem != null) {
+				lastPeeked = peekItem.toString();
+			}
 		}
+		return peekItem;
 	}
 
-	public CrawlURI nextUri() {
-		return uriQueue.poll();
+	/**
+	 * Remove the peekItem from the queue and adjusts the count.
+	 * 
+	 * @param frontier
+	 *            Work queues manager.
+	 */
+	protected void dequeue(final WorkQueueFrontier frontier, CrawlURI expected) {
+		try {
+			deleteItem(frontier, peekItem);
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		unpeek(expected);
+		count--;
+		lastDequeueTime = System.currentTimeMillis();
 	}
 
-	public long getWakeTime() {
-		return wakeTime;
+	/**
+	 * Forgive the peek, allowing a subsequent peek to return a different item.
+	 * 
+	 */
+	public void unpeek(CrawlURI expected) {
+		assert expected == peekItem : "unexpected peekItem";
+		peekItem = null;
 	}
 
-	public void setWakeTime(long wakeTime) {
-		this.wakeTime = wakeTime;
+	public long getDelay(TimeUnit unit) {
+		return unit.convert(getWakeTime() - System.currentTimeMillis(),
+				TimeUnit.MILLISECONDS);
 	}
 
 	public String getClassKey() {
 		return classKey;
 	}
+
+	public long getWakeTime() {
+		return wakeTime;
+	}
+	public void setWakeTime(long wakeTime) {
+		this.wakeTime = wakeTime;
+	}
+	
+	/**
+     * Insert the given curi, whether it is already present or not. 
+     * @param frontier WorkQueueFrontier.
+     * @param curi CrawlURI to insert.
+     * @throws IOException
+     */
+    private void insert(final WorkQueueFrontier frontier, CrawlURI curi,
+            boolean overwriteIfPresent)
+        throws IOException {
+        insertItem(frontier, curi, overwriteIfPresent);
+        lastQueued = curi.toString();
+    }
+
+    /**
+     * Insert the given curi, whether it is already present or not.
+     * Hook for subclasses. 
+     * 
+     * @param frontier WorkQueueFrontier.
+     * @param curi CrawlURI to insert.
+     * @throws IOException  if there was a problem while inserting the item
+     */
+    protected abstract void insertItem(final WorkQueueFrontier frontier,
+        CrawlURI curi, boolean overwriteIfPresent) throws IOException;
+
+	/**
+	 * Returns first item from queue (does not delete)
+	 * 
+	 * @return The peeked item, or null
+	 * @throws IOException
+	 *             if there was a problem while peeking
+	 */
+	protected abstract CrawlURI peekItem(final WorkQueueFrontier frontier)
+			throws IOException;
+
+	/**
+	 * Removes the given item from the queue.
+	 * 
+	 * This is only used to remove the first item in the queue, so it is not
+	 * necessary to implement a random-access queue.
+	 * 
+	 * @param frontier
+	 *            Work queues manager.
+	 * @throws IOException
+	 *             if there was a problem while deleting the item
+	 */
+	protected abstract void deleteItem(final WorkQueueFrontier frontier,
+			final CrawlURI item) throws IOException;
+
 }
