1253435a49b7a0ab9e487151788db54bd5ba41dc
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-08-17 23:20:23 +0000

    update cascade

--- eduburner-modular/EduBurner-Modular/EduBurner/app/src/main/java/eduburner/service/course/CourseManager.java
+++ eduburner-modular/EduBurner-Modular/EduBurner/app/src/main/java/eduburner/service/course/CourseManager.java
@@ -2,8 +2,6 @@ package eduburner.service.course;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,17 +11,12 @@ import eduburner.entity.course.Course;
 import eduburner.entity.course.CourseTag;
 import eduburner.entity.user.UserData;
 import eduburner.service.BaseManager;
-import eduburner.service.user.IUserManager;
 
 @SuppressWarnings("unchecked")
 @Component("courseManager")
 @Transactional
 public class CourseManager extends BaseManager implements ICourseManager {
-	
-	@Autowired
-	@Qualifier("userManager")
-	private IUserManager userManager;
-	
+
 	@Override
 	public void createCourse(Course course) {
 		createCourse(course, null, false);
@@ -37,22 +30,19 @@ public class CourseManager extends BaseManager implements ICourseManager {
 	@Override
 	public void createCourse(Course course, UserData creator,
 			boolean updateTagsString) {
-		
-		if(updateTagsString){
+
+		if (updateTagsString) {
 			updateCourseTagsForTagsString(course);
 		}
-		
-		if(creator != null){
+
+		if (creator != null) {
 			course.setCreator(creator);
 			course.addMemeber(creator);
-		}
-		
-		dao.save(course);
-		
-		if(creator != null){
 			creator.addCourse(course);
-			userManager.updateUserDate(creator);
 		}
+
+		dao.save(course);
+
 	}
 
 	@Override
@@ -78,9 +68,9 @@ public class CourseManager extends BaseManager implements ICourseManager {
 	@Override
 	public CourseTag getCourseTag(String tagName) {
 		List tags = dao.find("FROM CourseTag WHERE name= ?", tagName);
-		if(tags.size() > 0){
-			return (CourseTag)tags.get(0);
-		}else{
+		if (tags.size() > 0) {
+			return (CourseTag) tags.get(0);
+		} else {
 			return null;
 		}
 	}
@@ -88,9 +78,9 @@ public class CourseManager extends BaseManager implements ICourseManager {
 	@Override
 	public CourseTag getOrInsertCourseTag(String tagName) {
 		List tags = dao.find("FROM CourseTag WHERE name= ?", tagName);
-		if(tags.size() > 0){
-			return (CourseTag)tags.get(0);
-		}else{
+		if (tags.size() > 0) {
+			return (CourseTag) tags.get(0);
+		} else {
 			CourseTag ct = new CourseTag(tagName);
 			dao.save(ct);
 			return ct;
@@ -99,14 +89,14 @@ public class CourseManager extends BaseManager implements ICourseManager {
 
 	@Override
 	public void updateCourseTagsForTagsString(Course course) {
-		
+
 		List<CourseTag> tags = Lists.newArrayList();
-		
-		for(String t : course.getTagsStringList()){
+
+		for (String t : course.getTagsStringList()) {
 			CourseTag ct = getOrInsertCourseTag(t);
 			tags.add(ct);
 		}
-		
+
 		course.setTags(tags);
 	}
 
