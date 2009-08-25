2c6e02347f1df997f2a67e8f06c486b38be7877c
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-08-25 16:38:05 +0000

    code refactor

---
+++ eduburner-modular/EduBurner-Modular/EduBurner/app/src/main/java/eduburner/search/IEntrySearchService.java
@@ -0,0 +1,13 @@
+package eduburner.search;
+
+import java.util.List;
+
+import eduburner.entity.Entry;
+
+public interface IEntrySearchService {
+
+	void addEntryDocument(Entry entry);
+
+	void indexEntries(List<Entry> entries);
+
+}
--- eduburner-modular/EduBurner-Modular/EduBurner/app/src/main/java/eduburner/search/ISearchService.java
+++
@@ -1,9 +0,0 @@
-package eduburner.search;
-
-import java.util.List;
-
-public interface ISearchService {
-
-	List search(String fieldName, String textToSearch);
-
-}
