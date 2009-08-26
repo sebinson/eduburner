53979dc124ae59478024e95c2e3a576051927fce
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-08-26 14:38:47 +0000

    update search code

--- eduburner-modular/EduBurner-Modular/EduBurner/app/src/main/java/eduburner/web/controller/SearchController.java
+++ eduburner-modular/EduBurner-Modular/EduBurner/app/src/main/java/eduburner/web/controller/SearchController.java
@@ -2,14 +2,22 @@ package eduburner.web.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import eduburner.search.IEntrySearchService;
+
+@Controller
 public class SearchController {
 
 	private static final String SEARCH_RESULT_VIEW = "searchResult";
 	
-	//private ISearchService searchService;
+	@Autowired
+	@Qualifier("entrySearchService")
+	private IEntrySearchService entrySearchService;
 	
 	@RequestMapping(value="/search", method=RequestMethod.GET)
 	public String search(HttpServletRequest request){
