323df1ee83a140b9625a2343f159680253f4cf51
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-12-06 11:47:02 +0000

    code format

--- trunk/eduburner-modular/EduBurner-Modular/EduBurner/tools/src/main/java/torch/analysis/algorithm/AbstractAlgorithm.java
+++ trunk/eduburner-modular/EduBurner-Modular/EduBurner/tools/src/main/java/torch/analysis/algorithm/AbstractAlgorithm.java
@@ -1,137 +1,136 @@
 package torch.analysis.algorithm;
 
-import java.util.ArrayList;
-import java.util.List;
-
+import com.google.inject.Inject;
+import com.google.inject.name.Named;
 import torch.analysis.model.Chunk;
 import torch.analysis.model.Dictionary;
 import torch.analysis.model.Word;
 import torch.analysis.rule.IRule;
 
-import com.google.inject.Inject;
-import com.google.inject.name.Named;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA. User: rockmaple Date: 2009-12-3 Time: 22:09:19
  */
 public abstract class AbstractAlgorithm {
 
-	@Inject
-	@Named("MaxMatchRule")
-	protected IRule mmRule;
-	@Inject
-	@Named("LagestAvgWordLenRule")
-	protected IRule lawlRule;
-	@Inject
-	@Named("LagestAvgWordLenRule")
-	protected IRule svwlRule;
-	@Inject
-	@Named("LagestAvgWordLenRule")
-	protected IRule lsdmfocwRule;
-	@Inject
-	private Dictionary dictionary;
-	@Inject
-	@Named("maxWordLength")
-	private int maxWordLength;
+    @Inject
+    @Named("MaxMatchRule")
+    protected IRule mmRule;
+    @Inject
+    @Named("LagestAvgWordLenRule")
+    protected IRule lawlRule;
+    @Inject
+    @Named("LagestAvgWordLenRule")
+    protected IRule svwlRule;
+    @Inject
+    @Named("LagestAvgWordLenRule")
+    protected IRule lsdmfocwRule;
+    @Inject
+    private Dictionary dictionary;
+    @Inject
+    @Named("maxWordLength")
+    private int maxWordLength;
 
-	protected int index = 0;
+    protected int index = 0;
 
-	public Word next(char[] chars) {
+    public Word next(char[] chars) {
 
-		if (index >= chars.length) {
-			index = 0;
-			return null;
-		}
-		char current = chars[index];
-		if (isBasicLatin(current)) {
-			Word word = getBasicLatinWord(chars, index);
-			index += word.getLength();
-			return word;
-		} else {
-			Chunk[] chunks = createChunks(chars, index);
-			Word word = getCJKWord(chunks);
-			index += word.getLength();
-			return word;
-		}
-	}
+        if (index >= chars.length) {
+            index = 0;
+            return null;
+        }
+        char current = chars[index];
+        if (isBasicLatin(current)) {
+            Word word = getBasicLatinWord(chars, index);
+            index += word.getLength();
+            return word;
+        } else {
+            Chunk[] chunks = createChunks(chars, index);
+            Word word = getCJKWord(chunks);
+            index += word.getLength();
+            return word;
+        }
+    }
 
-	protected Word getBasicLatinWord(char[] chars, int index) {
-		StringBuffer basicLatinWord = new StringBuffer();
-		while ((index < chars.length) && isBasicLatin(chars[index])) {
-			if (Character.isWhitespace(chars[index])) {
-				if (basicLatinWord.length() == 0) {
-					basicLatinWord.append(chars[index]);
-					index++;
-					return new Word(basicLatinWord.toString(),
-							Word.BASICLATIN_WORD);
-				} else {
-					return new Word(basicLatinWord.toString(),
-							Word.BASICLATIN_WORD);
-				}
-			}
-			basicLatinWord.append(chars[index]);
-			index++;
-		}
+    protected Word getBasicLatinWord(char[] chars, int index) {
+        StringBuffer basicLatinWord = new StringBuffer();
+        while ((index < chars.length) && isBasicLatin(chars[index])) {
+            if (Character.isWhitespace(chars[index])) {
+                if (basicLatinWord.length() == 0) {
+                    basicLatinWord.append(chars[index]);
+                    index++;
+                    return new Word(basicLatinWord.toString(),
+                            Word.BASICLATIN_WORD);
+                } else {
+                    return new Word(basicLatinWord.toString(),
+                            Word.BASICLATIN_WORD);
+                }
+            }
+            basicLatinWord.append(chars[index]);
+            index++;
+        }
 
-		return new Word(basicLatinWord.toString(), Word.BASICLATIN_WORD);
-	}
+        return new Word(basicLatinWord.toString(), Word.BASICLATIN_WORD);
+    }
 
-	protected abstract Chunk[] createChunks(char[] chars, int index2);
+    protected abstract Chunk[] createChunks(char[] chars, int index2);
 
-	protected Word[] findMatchWords(char[] chars, int index) {
+    protected Word[] findMatchWords(char[] chars, int index) {
 
-		char c = chars[index];
+        char c = chars[index];
 
-		List matchWords = new ArrayList();
+        List matchWords = new ArrayList();
 
-		StringBuilder sb = new StringBuilder();
-		sb.append(c);
+        StringBuilder sb = new StringBuilder();
+        sb.append(c);
 
-		String wordValue = sb.toString();
-		if (dictionary.isMatched(wordValue)) {
-			matchWords.add(dictionary.getWord(wordValue));
-		}
+        String wordValue = sb.toString();
+        if (dictionary.isMatched(wordValue)) {
+            matchWords.add(dictionary.getWord(wordValue));
+        }
 
-		for (int i = 1; i < maxWordLength && ((i + index) < chars.length); i++) {
-			if (isBasicLatin(chars[i + index]))
-				break;
-			sb.append(chars[i + index]);
-			String temp = sb.toString();
-			if (dictionary.isMatched(temp)) {
-				matchWords.add(dictionary.getWord(temp));
-			}
-		}
+        for (int i = 1; i < maxWordLength && ((i + index) < chars.length); i++) {
+            if (isBasicLatin(chars[i + index]))
+                break;
+            sb.append(chars[i + index]);
+            String temp = sb.toString();
+            if (dictionary.isMatched(temp)) {
+                matchWords.add(dictionary.getWord(temp));
+            }
+        }
 
-		if (matchWords.isEmpty()) {
-			matchWords.add(new Word(wordValue, Word.UNRECOGNIZED));
-		}
+        if (matchWords.isEmpty()) {
+            matchWords.add(new Word(wordValue, Word.UNRECOGNIZED));
+        }
 
-		Word[] words = new Word[matchWords.size()];
-		matchWords.toArray(words);
+        Word[] words = new Word[matchWords.size()];
+        matchWords.toArray(words);
 
-		matchWords.clear();
+        matchWords.clear();
 
-		return words;
-	}
+        return words;
+    }
 
-	protected Word getCJKWord(Chunk[] chunks) {
-		Chunk[] chunkList = mmRule.applyRule(chunks);
+    protected Word getCJKWord(Chunk[] chunks) {
+        Chunk[] chunkList = mmRule.applyRule(chunks);
 
-		if (chunkList.length >= 2) {
-			chunkList = lawlRule.applyRule(chunkList);
-			if (chunkList.length >= 2) {
-				chunkList = svwlRule.applyRule(chunkList);
-				if (chunkList.length >= 2) {
-					chunkList = lsdmfocwRule.applyRule(chunkList);
-				}
-			}
-		}
+        if (chunkList.length >= 2) {
+            chunkList = lawlRule.applyRule(chunkList);
+            if (chunkList.length >= 2) {
+                chunkList = svwlRule.applyRule(chunkList);
+                if (chunkList.length >= 2) {
+                    chunkList = lsdmfocwRule.applyRule(chunkList);
+                }
+            }
+        }
 
-		Chunk chunk = chunkList[0];
-		return chunk.getWords()[0];
-	}
+        Chunk chunk = chunkList[0];
+        return chunk.getWords()[0];
+    }
 
-	private boolean isBasicLatin(char c) {
-		return Character.UnicodeBlock.of(c) == Character.UnicodeBlock.BASIC_LATIN;
-	}
+    private boolean isBasicLatin(char c) {
+        return Character.UnicodeBlock.of(c) == Character.UnicodeBlock.BASIC_LATIN;
+    }
 }
