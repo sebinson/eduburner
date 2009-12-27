facaed69038511ad09d486c28a421d073e644722
rockmaple <rockmaple@2475d89d-5d55-0410-a5f0-250126501250>
2009-12-27 14:51:06 +0000

    init add burnerweb

---
+++ burnerweb/src/main/scala/burnerweb/scala/controller/Start.scala
@@ -0,0 +1,14 @@
+package burnerweb.scala.controller
+
+import com.google.sitebricks.{Show, At}
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: rockmaple
+ * Date: 2009-12-27
+ * Time: 13:44:23
+ */
+@At("/") @Show("index.ftl")
+class Start{
+  
+}
